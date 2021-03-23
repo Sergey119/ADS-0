ee11e2af799c055f03f4030a3bb5dfc61a3848c8
Sergey119 <78952666+Sergey119@users.noreply.github.com>
2021-03-23 14:12:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 int gcd(int a, int b) {
         if (b == 0)
-                return a;
+                { return a; }
         else
-                return gcd (b, a % b);
+                {return gcd (b, a % b);}
 }
